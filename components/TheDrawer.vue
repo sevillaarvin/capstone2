00762707cf1385790aeaad2aac2a716e52a2bc58
aes <sevillaarvin@gmail.com>
2018-11-20 06:59:49 +0800

    Prepare navigation for store.

---
+++ components/TheDrawer.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-navigation-drawer
+    :value="value"
+    fixed
+    temporary
+    @input="$emit('input', $event)">
+    <v-img />
+  </v-navigation-drawer>
+</template>
+
+<script>
+  export default {
+    props: {
+      value: {
+        type: Boolean,
+        default: false
+      }
+    },
+    data() {
+      return {
+      }
+    }
+  }
+</script>
+
+<style>
+</style>
---
+++ components/TheHeader.vue
@@ -0,0 +1,89 @@
+<template>
+  <v-toolbar>
+    <v-toolbar-side-icon
+      @click="$emit('drawer')"/>
+    <v-container
+      fluid
+      fill-height
+      d-flex
+      justify-start>
+      <v-toolbar-items>
+        <v-btn
+          v-for="nav in genNavs"
+          :key="nav.name"
+          :to="nav.path"
+          flat>
+          {{ nav.name }}
+        </v-btn>
+      </v-toolbar-items>
+      <v-toolbar-title>
+        TITLE
+      </v-toolbar-title>
+      <v-toolbar-items>
+        <v-btn
+          v-for="nav in userNavs"
+          :key="nav.name"
+          :to="nav.path"
+          flat>
+          {{ nav.name }}
+        </v-btn>
+      </v-toolbar-items>
+    </v-container>
+  </v-toolbar>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        genNavs: [
+          {
+            name: "News",
+            path: "/news"
+          },
+          {
+            name: "Events",
+            path: "/events"
+          },
+          {
+            name: "Facilities",
+            path: "/facilities"
+          },
+          {
+            name: "Membership",
+            path: "/membership"
+          },
+          {
+            name: "Store",
+            path: "/store"
+          },
+          {
+            name: "About",
+            path: "/about"
+          },
+        ],
+        userNavs: [
+          {
+            name: "Cart",
+            path: "/cart"
+          },
+          {
+            name: "Signin",
+            path: "/signin"
+          },
+          {
+            name: "Signup",
+            path: "/signup"
+          },
+          {
+            name: "Signout",
+            path: "/signout"
+          },
+        ]
+      }
+    },
+  }
+</script>
+
+<style>
+</style>

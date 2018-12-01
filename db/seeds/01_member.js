5634e49bb9ba75be40ce17d5d79f3364f2eea1f7
aes <sevillaarvin@gmail.com>
2018-12-01 10:58:03 +0800

    Fix asynchronous bcrypt in seed file.

--- db/seeds/01_member.js
+++ db/seeds/01_member.js
@@ -1,10 +1,16 @@
 const bcrypt = require("bcryptjs")
 
-exports.seed = function(knex, Promise) {
-  let password
-  bcrypt.hash("password1", 10, (err, hash) => {
-    password = hash
-  })
+exports.seed = async function(knex, Promise) {
+  let password1
+  let password2
+  let password3
+  try {
+    password1 = await bcrypt.hash("password1", 10)
+    password2 = await bcrypt.hash("password2", 10)
+    password3 = await bcrypt.hash("password3", 10)
+  } catch (e) {
+    return Promise.reject(e)
+  }
   // Deletes ALL existing entries
   return knex('member').del()
     .then(function () {
@@ -15,21 +21,21 @@ exports.seed = function(knex, Promise) {
           lastName: "Smith",
           email: "john@smith.com",
           username: "smithjohn",
-          password,
+          password: password1,
         },
         {
           firstName: "Jaden",
           lastName: "Smith",
           email: "jaden@smith.com",
           username: "smithjaden",
-          password,
+          password: password2,
         },
         {
           firstName: "Juan",
           lastName: "Felipe",
           email: "juan@felipe.com",
           username: "felipejuan",
-          password,
+          password: password3,
         },
       ]);
     });

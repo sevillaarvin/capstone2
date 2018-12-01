5d4d4e2a61a2df70092e6c6d170d0637650ea2f3
aes <sevillaarvin@gmail.com>
2018-12-01 08:11:13 +0800

    Refactor deploy script.

--- knexfile.js
+++ knexfile.js
@@ -1,14 +1,12 @@
 module.exports = {
   development: {
     client: "pg",
-    // connection: "postgres://fwsxzkihaienjw:b0be65b967e79d1e33d547a3f8adb305508a0134cd0b5bcc974073cb779ec45a@ec2-54-163-230-178.compute-1.amazonaws.com:5432/dd1u5gl9f83kvt",
-    // connection: process.env.DATABASE_URL || "postgres://aes:password@localhost/clubseville_test",
     connection: {
       host: "localhost",
       user: "aes",
       password: "password",
-      database: "clubseville",
-      charset: "utf9"
+      database: "nextseville",
+      charset: "utf8"
     },
     migrations: {
       directory: __dirname + '/db/migrations'
